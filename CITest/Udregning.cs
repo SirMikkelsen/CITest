661bf195a860b7caa05e96d716daebda4aa3dd83
Gordon <gordonmikkelsen87@gmail.com>
2018-08-20 09:44:46 +0200

    Create test
    create test

---
+++ CITest/Udregning.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITest
+{
+   public class Udregning
+    {
+        public int Add(int x , int y)
+        {
+
+            return x + y;
+        }
+
+
+
+        public int Subtract(int x, int y)
+        {
+
+            return x - y;
+        }
+
+    }
+}
